9d0349afecc9af006580ad3afc2f5724974325e6
AgronAsani <agron.asani@students.hevs.ch>
2019-06-01 19:04:18 +0200

    finito

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,7 @@ import {createDashboardLayer} from './layers/dashboard.js';
 function createPlayerEnv(playerEntity) {
     const playerEnv = new Entity();
     const playerControl = new PlayerController();
-    playerControl.checkpoint.set(5216, 64);
+    playerControl.checkpoint.set(64, 64);
     playerControl.setPlayer(playerEntity);
     playerEnv.addTrait(playerControl);
     return playerEnv;
