2b4fe62e898e1e9e793f34d40ee8c0be0e936b0b
YannJollien <48057181+YannJollien@users.noreply.github.com>
2019-06-02 13:54:01 +0200

    finish - 4

--- public/js/traits/PlayerController.js
+++ public/js/traits/PlayerController.js
@@ -24,31 +24,40 @@ export default class PlayerController extends Trait {
             this.player.killable.revive();
             this.player.pos.set(this.checkpoint.x, this.checkpoint.y);
             level.entities.add(this.player);
-        } else {
+        } else if (level.entities.has(this.player)) {
             this.time += deltaTime * 2;
-            if (this.player.pos.x >= 6200 && this.player.pos.x <=6210) {
-                this.highscore = this.time;
-                console.log(this.highscore);
-                document.getElementById("test").innerHTML = this.highscore.toFixed().toString().padStart(1, '0') + " seconds";
-                var btn = document.getElementById("button");
-                btn.style.display = 'block';
-                setTimeout(this.end, 10000);
-            }
+        }  if (this.player.pos.x >= 6200 && this.player.pos.x <=6210){
+            this.highscore = this.time;
+            console.log(this.highscore);
+            document.getElementById("test").innerHTML = this.highscore.toFixed().toString().padStart(1, '0') + " seconds!!!";
+            var btn = document.getElementById("button");
+            btn.style.display = 'block';
+            document.onkeydown = function(ev)
+            {
+                if(event.code === 'KeyA' || event.code === 'KeyD')
+                {
+                    ev.cancelBubble = true;
+                    ev.returnValue = false;
+                }
 
+            }
+            btn.addEventListener("click",this.replay);
+            setTimeout(this.end, 10000);
         }
     }
-
-
 
     end() {
         window.open('http://localhost:63342/super-mario/public/menu.html', '_self');
         return;
     }
 
-    replay() {
-        this.player.pos.set(2900, 64);
-        console.log("Yes");
+    replay(){
+        window.open('http://localhost:63342/super-mario/public/index.html', '_self');
+        return;
+        console.log("Test");
     }
+
+
 
 
     level2(entity, deltaTime, level) {
